19f9bb8911fd169f77d308e320a188b84f3d3fb9
aayushraj07 <aayush.97raj@gmail.com>
2020-07-21 22:57:45 +0530

    react part done , routes made

---
+++ client/components/single.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from "react-router";
+
+class Single extends Component {
+  render() {
+    return <div className="single-photo">I am single</div>;
+  }
+}
+
+export default Single;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -3,4 +3,19 @@ import { render } from "react-dom";
 
 import css from "./styles/style.styl";
 
-render(<p>Hello</p>, document.getElementById("root"));
+import Main from "./components/Main";
+import Single from "./components/single";
+import PhotoGrid from "./components/PhotoGrid";
+
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById("root"));
